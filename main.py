e24b6ea1da5ad1276a96f7181203e7767090037c
dizzt <eorhkd69@gmail.com>
2020-12-13 20:17:53 +0900

    Update main.py
    도움말 업데이트, 중국어 간체자 추가

--- main.py
+++ main.py
@@ -118,9 +118,52 @@ async def on_message(message): # on_message() event : when the bot has recieved 
 
     if message.content.startswith("/help"):
         #최종 결과 임베드 타입으로 출력
+        embed = discord.Embed(title="**언어를 골라주세요!**", description="*언어를 골라주세요!*", color=0x009900)
+        embed.add_field(name="/k help", value="한국어로 적힌 도움말을 볼 수 있습니다.", inline=False)
+        embed.add_field(name="/e help", value="Help is available in English.", inline=False)
+        embed.add_field(name="/c help", value="您可以获得中文的帮助。", inline=False)
+        embed.set_thumbnail(url=thumb_url)
+        embed.set_footer(text=under_text, icon_url=under_icon_url)
+        await message.channel.send(embed=embed)
+
+
+
+    #한국어 도움말
+    if message.content.startswith("/k help"):
+        #최종 결과 임베드 타입으로 출력
         embed = discord.Embed(title=":regional_indicator_h: :regional_indicator_e: :regional_indicator_l: :regional_indicator_p:", description="*이 디스코드 봇을 사용하기 위한 설명서*", color=0x009900)
         embed.add_field(name="도움말", value="`/help`를 통해 도움말 페이지를 열 수 있습니다.", inline=False)
-        embed.add_field(name="한국어 :left_right_arrow: 영어", value="`/k2e <text>`를 통해 한국어를 영어로, `/k2e <text>`를 통해 영어를 한국어로 번역할 수 있습니다.", inline=False)
+        embed.add_field(name="한국어 :left_right_arrow: 영어", value="`/k2e <text>`를 통해 한국어를 영어로, `/e2k <text>`를 통해 영어를 한국어로 번역할 수 있습니다.", inline=False)
+        embed.add_field(name="한국어 :left_right_arrow: 영어", value="`/k2c <text>`를 통해 한국어를 중국어로, `/c2k <text>`를 통해 중국어를 한국어로 번역할 수 있습니다.", inline=False)
+        embed.add_field(name="한국어 :left_right_arrow: 영어", value="`/e2c <text>`를 통해 영어를 중어로, `/c2e <text>`를 통해 중국어를 영어로 번역할 수 있습니다.", inline=False)
+        embed.set_thumbnail(url=thumb_url)
+        embed.set_footer(text=under_text, icon_url=under_icon_url)
+        await message.channel.send(embed=embed)
+
+
+
+    #영어 도움말
+    if message.content.startswith("/e help"):
+        #최종 결과 임베드 타입으로 출력
+        embed = discord.Embed(title=":regional_indicator_h: :regional_indicator_e: :regional_indicator_l: :regional_indicator_p:", description="*Instructions for using this Discord Bot*", color=0x009900)
+        embed.add_field(name="도움말", value="`/help`를 통해 도움말 페이지를 열 수 있습니다.", inline=False)
+        embed.add_field(name="Korean :left_right_arrow: English", value="You can translate Korean to English through `/k2e <text>` and English to Korean through `/e2k <text>`.", inline=False)
+        embed.add_field(name="Korean :left_right_arrow: Simplified Chinese", value="You can translate Korean to Chinese through `/k2c <text>` and Chinese to Korean through `/c2k <text>`.", inline=False)
+        embed.add_field(name="English :left_right_arrow: Simplified Chinese", value="You can translate English to Chinese with `/e2c <text>` and Chinese to English with `/c2e <text>`.", inline=False)
+        embed.set_thumbnail(url=thumb_url)
+        embed.set_footer(text=under_text, icon_url=under_icon_url)
+        await message.channel.send(embed=embed)
+
+
+
+    #중국어 도움말
+    if message.content.startswith("/c help"):
+        #최종 결과 임베드 타입으로 출력
+        embed = discord.Embed(title=":regional_indicator_h: :regional_indicator_e: :regional_indicator_l: :regional_indicator_p:", description="*使用该Discord Bot的说明*", color=0x009900)
+        embed.add_field(name="도움말", value="`/help`를 통해 도움말 페이지를 열 수 있습니다.", inline=False)
+        embed.add_field(name="韩语 :left_right_arrow: 英语", value="您可以通过`/k2e <text>`将韩语翻译为英语，并通过`/e2k <text>`将英语翻译为韩语。", inline=False)
+        embed.add_field(name="韩语 :left_right_arrow: 简体中文", value="您可以通过`/ k2c <text>`将韩语翻译成中文，并通过`/ c2k <text>`将汉语译成韩语。", inline=False)
+        embed.add_field(name="英语 :left_right_arrow: 简体中文", value="您可以使用`/e2c <text>`将英语翻译为中文，使用`/c2e <text>`将英语翻译为中文。", inline=False)
         embed.set_thumbnail(url=thumb_url)
         embed.set_footer(text=under_text, icon_url=under_icon_url)
         await message.channel.send(embed=embed)
@@ -243,6 +286,270 @@ async def on_message(message): # on_message() event : when the bot has recieved 
                     #최종 결과 임베드 타입으로 출력
                     translatedText = api_callResult['message']['result']["translatedText"]
                     embed = discord.Embed(title="English :arrow_right: Korean", description="", color=0x009900)
+                    embed.add_field(name="Before", value=savedCombineword, inline=False)
+                    embed.add_field(name="After", value=translatedText, inline=False)
+                    embed.set_thumbnail(url=thumb_url)
+                    embed.set_footer(text=under_text, icon_url=under_icon_url)
+
+                    #예외 발생 판정
+                    await message.channel.send(trans_1, embed=embed)
+                else:
+                    await message.channel.send(trans_0 + responsedCode)
+
+        #예외 처리
+        except HTTPError as e:
+            await message.channel.send(error_0)
+
+
+
+#한국어 -> 중국어 간체 번역기
+
+    if message.content.startswith("/k2c"):
+        baseurl = "https://openapi.naver.com/v1/papago/n2mt"
+        
+        #split처리 -> [1:]을 for문으로 붙인다.
+        trsText = message.content.split(" ")
+        
+        try:
+            if len(trsText) == 1:
+                await message.channel.send(error_text)
+                
+            else:
+                trsText = trsText[1:]
+                combineword = ""
+                for word in trsText:
+                    combineword += " " + word
+                    
+                #번역할 문장 저장
+                savedCombineword = combineword.strip()
+                combineword = quote(savedCombineword)
+
+                #번역할 문장 IDLE 출력
+                print(combineword)
+                
+                #문자열 쿼리 생성 (여기에서 번역 전과 후의 언어를 설정할 수 있음)
+                dataParmas = "source=ko&target=zh-CN&text=" + combineword
+                
+                #리퀘스트 생성
+                request = Request(baseurl)
+                
+                #헤더 추가
+                request.add_header("X-Naver-Client-Id", client_id)
+                request.add_header("X-Naver-Client-Secret", client_secret)
+                response = urlopen(request, data=dataParmas.encode("utf-8"))
+
+                responsedCode = response.getcode()
+                
+                if (responsedCode == 200):
+                    response_body = response.read()
+                    
+                    #response_body -> byte string : decode to utf-8
+                    api_callResult = response_body.decode('utf-8')
+                    
+                    #JSON 데이터는 문자열 유형으로 인쇄되므로, JSON 사전과 같은 유형으로 재생성 해야합니다.
+                    api_callResult = json.loads(api_callResult)
+                    
+                    #최종 결과 임베드 타입으로 출력
+                    translatedText = api_callResult['message']['result']["translatedText"]
+                    embed = discord.Embed(title="Korean :arrow_right: Simplified Chinese", description="", color=0x009900)
+                    embed.add_field(name="Before", value=savedCombineword, inline=False)
+                    embed.add_field(name="After", value=translatedText, inline=False)
+                    embed.set_thumbnail(url=thumb_url)
+                    embed.set_footer(text=under_text, icon_url=under_icon_url)
+
+                    #예외 발생 판정
+                    await message.channel.send(trans_1, embed=embed)
+                else:
+                    await message.channel.send(trans_0 + responsedCode)
+
+        #예외 처리
+        except HTTPError as e:
+            await message.channel.send(error_0)
+
+
+
+#중국어 간체 -> 한국어 번역기
+
+    if message.content.startswith("/c2k"):
+        baseurl = "https://openapi.naver.com/v1/papago/n2mt"
+        
+        #split처리 -> [1:]을 for문으로 붙인다.
+        trsText = message.content.split(" ")
+        
+        try:
+            if len(trsText) == 1:
+                await message.channel.send(error_text)
+                
+            else:
+                trsText = trsText[1:]
+                combineword = ""
+                for word in trsText:
+                    combineword += " " + word
+                    
+                #번역할 문장 저장
+                savedCombineword = combineword.strip()
+                combineword = quote(savedCombineword)
+
+                #번역할 문장 IDLE 출력
+                print(combineword)
+                
+                #문자열 쿼리 생성 (여기에서 번역 전과 후의 언어를 설정할 수 있음)
+                dataParmas = "source=zh-CN&target=ko&text=" + combineword
+                
+                #리퀘스트 생성
+                request = Request(baseurl)
+                
+                #헤더 추가
+                request.add_header("X-Naver-Client-Id", client_id)
+                request.add_header("X-Naver-Client-Secret", client_secret)
+                response = urlopen(request, data=dataParmas.encode("utf-8"))
+
+                responsedCode = response.getcode()
+                
+                if (responsedCode == 200):
+                    response_body = response.read()
+                    
+                    #response_body -> byte string : decode to utf-8
+                    api_callResult = response_body.decode('utf-8')
+                    
+                    #JSON 데이터는 문자열 유형으로 인쇄되므로, JSON 사전과 같은 유형으로 재생성 해야합니다.
+                    api_callResult = json.loads(api_callResult)
+                    
+                    #최종 결과 임베드 타입으로 출력
+                    translatedText = api_callResult['message']['result']["translatedText"]
+                    embed = discord.Embed(title="Simplified Chinese :arrow_right: Korean", description="", color=0x009900)
+                    embed.add_field(name="Before", value=savedCombineword, inline=False)
+                    embed.add_field(name="After", value=translatedText, inline=False)
+                    embed.set_thumbnail(url=thumb_url)
+                    embed.set_footer(text=under_text, icon_url=under_icon_url)
+
+                    #예외 발생 판정
+                    await message.channel.send(trans_1, embed=embed)
+                else:
+                    await message.channel.send(trans_0 + responsedCode)
+
+        #예외 처리
+        except HTTPError as e:
+            await message.channel.send(error_0)
+
+
+
+#영어 -> 중국어 간체 번역기
+
+    if message.content.startswith("/e2c"):
+        baseurl = "https://openapi.naver.com/v1/papago/n2mt"
+        
+        #split처리 -> [1:]을 for문으로 붙인다.
+        trsText = message.content.split(" ")
+        
+        try:
+            if len(trsText) == 1:
+                await message.channel.send(error_text)
+                
+            else:
+                trsText = trsText[1:]
+                combineword = ""
+                for word in trsText:
+                    combineword += " " + word
+                    
+                #번역할 문장 저장
+                savedCombineword = combineword.strip()
+                combineword = quote(savedCombineword)
+
+                #번역할 문장 IDLE 출력
+                print(combineword)
+                
+                #문자열 쿼리 생성 (여기에서 번역 전과 후의 언어를 설정할 수 있음)
+                dataParmas = "source=en&target=zh-CN&text=" + combineword
+                
+                #리퀘스트 생성
+                request = Request(baseurl)
+                
+                #헤더 추가
+                request.add_header("X-Naver-Client-Id", client_id)
+                request.add_header("X-Naver-Client-Secret", client_secret)
+                response = urlopen(request, data=dataParmas.encode("utf-8"))
+
+                responsedCode = response.getcode()
+                
+                if (responsedCode == 200):
+                    response_body = response.read()
+                    
+                    #response_body -> byte string : decode to utf-8
+                    api_callResult = response_body.decode('utf-8')
+                    
+                    #JSON 데이터는 문자열 유형으로 인쇄되므로, JSON 사전과 같은 유형으로 재생성 해야합니다.
+                    api_callResult = json.loads(api_callResult)
+                    
+                    #최종 결과 임베드 타입으로 출력
+                    translatedText = api_callResult['message']['result']["translatedText"]
+                    embed = discord.Embed(title="English :arrow_right: Simplified Chinese", description="", color=0x009900)
+                    embed.add_field(name="Before", value=savedCombineword, inline=False)
+                    embed.add_field(name="After", value=translatedText, inline=False)
+                    embed.set_thumbnail(url=thumb_url)
+                    embed.set_footer(text=under_text, icon_url=under_icon_url)
+
+                    #예외 발생 판정
+                    await message.channel.send(trans_1, embed=embed)
+                else:
+                    await message.channel.send(trans_0 + responsedCode)
+
+        #예외 처리
+        except HTTPError as e:
+            await message.channel.send(error_0)
+
+
+
+#중국어 간체 -> 영어 번역기
+
+    if message.content.startswith("/c2e"):
+        baseurl = "https://openapi.naver.com/v1/papago/n2mt"
+        
+        #split처리 -> [1:]을 for문으로 붙인다.
+        trsText = message.content.split(" ")
+        
+        try:
+            if len(trsText) == 1:
+                await message.channel.send(error_text)
+                
+            else:
+                trsText = trsText[1:]
+                combineword = ""
+                for word in trsText:
+                    combineword += " " + word
+                    
+                #번역할 문장 저장
+                savedCombineword = combineword.strip()
+                combineword = quote(savedCombineword)
+
+                #번역할 문장 IDLE 출력
+                print(combineword)
+                
+                #문자열 쿼리 생성 (여기에서 번역 전과 후의 언어를 설정할 수 있음)
+                dataParmas = "source=zh-CN&target=en&text=" + combineword
+                
+                #리퀘스트 생성
+                request = Request(baseurl)
+                
+                #헤더 추가
+                request.add_header("X-Naver-Client-Id", client_id)
+                request.add_header("X-Naver-Client-Secret", client_secret)
+                response = urlopen(request, data=dataParmas.encode("utf-8"))
+
+                responsedCode = response.getcode()
+                
+                if (responsedCode == 200):
+                    response_body = response.read()
+                    
+                    #response_body -> byte string : decode to utf-8
+                    api_callResult = response_body.decode('utf-8')
+                    
+                    #JSON 데이터는 문자열 유형으로 인쇄되므로, JSON 사전과 같은 유형으로 재생성 해야합니다.
+                    api_callResult = json.loads(api_callResult)
+                    
+                    #최종 결과 임베드 타입으로 출력
+                    translatedText = api_callResult['message']['result']["translatedText"]
+                    embed = discord.Embed(title="Simplified Chinese :arrow_right: English", description="", color=0x009900)
                     embed.add_field(name="Before", value=savedCombineword, inline=False)
                     embed.add_field(name="After", value=translatedText, inline=False)
                     embed.set_thumbnail(url=thumb_url)
